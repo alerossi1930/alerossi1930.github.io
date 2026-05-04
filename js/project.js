25d5aa01b60fc3bda71b33e51c9a0481f0a6a1e8
alerossi1930 <alerossi1930@gmail.com>
2026-05-04 08:38:54 +0200

    design

--- js/project.js
+++ js/project.js
@@ -1,26 +1,46 @@
 const lightbox = document.getElementById('lightbox');
 const lightboxImg = document.getElementById('lightboxImg');
+const lightboxVideo = document.getElementById('lightboxVideo');
 const closeBtn = document.getElementById('lightboxClose');
 
-const imgs = document.querySelectorAll('.project-gallery img');
+const imgs = document.querySelectorAll('.project-gallery img, .project-gallery video');
 
-imgs.forEach(img => {
-  img.addEventListener('click', () => {
+imgs.forEach(el => {
+  el.addEventListener('click', () => {
     lightbox.style.display = 'flex';
-    lightboxImg.src = img.src;
     document.body.style.overflow = 'hidden';
+
+    if (el.tagName === 'IMG') {
+      lightboxImg.src = el.src;
+      lightboxImg.style.display = 'block';
+
+      lightboxVideo.style.display = 'none';
+      lightboxVideo.pause();
+    }
+
+    if (el.tagName === 'VIDEO') {
+      lightboxVideo.src = el.src;
+      lightboxVideo.style.display = 'block';
+      lightboxVideo.play();
+
+      lightboxImg.style.display = 'none';
+    }
   });
 });
 
 closeBtn.addEventListener('click', () => {
   lightbox.style.display = 'none';
   document.body.style.overflow = '';
+
+  lightboxVideo.pause();
 });
 
 lightbox.addEventListener('click', (e) => {
   if(e.target === lightbox){
     lightbox.style.display = 'none';
     document.body.style.overflow = '';
+
+    lightboxVideo.pause();
   }
 });
 
